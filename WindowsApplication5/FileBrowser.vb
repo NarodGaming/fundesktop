4bf63d9e6b689d2ff25a24fda7261636dd67d5e4
Jukebox Guy <jamie.manu1@gmail.com>
2015-10-17 22:34:31 +0100

    Added GUI for file browser - Just GUI!

--- WindowsApplication5/FileBrowser.vb
+++ WindowsApplication5/FileBrowser.vb
@@ -2,4 +2,8 @@
     Private Sub FileBrowser_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         Desktop.SetupClosing("File Browser")
     End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
 End Class
